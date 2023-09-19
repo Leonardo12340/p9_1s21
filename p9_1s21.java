c70c92aa4bf6cfe44b343747a0a68d701e9add80
Leonardo12340 <144502307+Leonardo12340@users.noreply.github.com>
2023-09-19 15:48:35 -0600

    Add files via upload

---
+++ p9_1s21.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class p9_1s21{
+	public stattic void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System operacion;
+		System.out.println("Que tipo de operaciones desea realizar");
+		System.out.println("escribala: Suma, Resta");
+		operacion = teclado.nextLine()
+
+		if (operacion.equals("Suma")){
+			System.out.println("Haremos una suma");
+		}else if (operacion.equals("Resta")){
+			System.out.println("Haremos una resta");
+		}else {
+			System.out.println("No existe la operacon")
+		}
+	}
+}
