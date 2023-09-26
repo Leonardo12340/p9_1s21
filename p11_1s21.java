b893dbc68d13a0d3e6a2de0d40b57459f02990c0
Leonardo12340 <144502307+Leonardo12340@users.noreply.github.com>
2023-09-26 15:29:37 -0600

    Add files via upload

---
+++ p11_1s21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P11_1s21{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String operacion;
+		System.out.println("Quetipo de operacion desea realizar");
+		System.out.println("ingresa una opcion [Suma,Resta,Multiplicacion,Divicion,Potencia,Raiz]: ");
+		String opc="Suma";
+		opc=teclado.nextLine();
+		switch(opc){
+		case "Suma":
+			if (operacion.equals("Suma")){
+				System.out.println("Haremos una suma");
+				
+				Scanner input = new Scanner(System.in);
+			}
+
+	}
+}
